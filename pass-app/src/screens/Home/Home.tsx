4cfafbabddd1da696f38e310913a1e5bd971aa47
alv3sg <gabriel.alves.rosario@gmail.com>
2024-01-19 15:21:42 +0000

    Separacao-de-responsabilidades

--- pass-app/src/screens/Home/Home.tsx
+++ pass-app/src/screens/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import  styles from "./Styles";
 
 
 //funcao que ira retornar o componente
@@ -13,12 +13,3 @@ export default function Home() {
         </View>
     )
 }
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  
