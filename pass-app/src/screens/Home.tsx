ce1afe1aa404d6d615dbc649a27fdea1fb392e4f
alv3sg <gabriel.alves.rosario@gmail.com>
2024-01-19 15:16:29 +0000

    primeira-view

---
+++ pass-app/src/screens/Home.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+//funcao que ira retornar o componente
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
