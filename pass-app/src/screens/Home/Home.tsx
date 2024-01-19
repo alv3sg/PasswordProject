c2d96b41f7067d77b9b8cbe0efc78cc943d257bd
alv3sg <gabriel.alves.rosario@gmail.com>
2024-01-19 15:29:02 +0000

    Criando-Componente

--- pass-app/src/screens/Home/Home.tsx
+++ pass-app/src/screens/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import  styles from "./Styles";
+import { Menu } from "../../components/Menu/Menu";
 
 
 //funcao que ira retornar o componente
 export default function Home() {
     return (
         <View style={styles.container}>
+            <Menu />
             <Text>Open up App.tsx to start working on your app!</Text>
             <StatusBar style="auto" />
         </View>
